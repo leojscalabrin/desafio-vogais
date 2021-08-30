26ce268552181a74abbeda20a68a827145255538
leojscalabrin <74567440+leojscalabrin@users.noreply.github.com>
2021-08-30 20:27:13 -0300

    correções

--- index.js
+++ index.js
@@ -3,33 +3,28 @@ let $text = document.querySelector('.text')
 let $resultado = document.querySelector('.resultado')
 
 function contaVogais(texto){
-    
     let vogais = 0;
     let string = texto.toString();
 
     for (var i = 0; i <= string.length - 1; i++){
-        if (string.charAt(i) === "a" ||
-        string.charAt(i) === "e" ||
-        string.charAt(i) === "i" ||
-        string.charAt(i) === "o" ||
-        string.charAt(i) === "u" ||
-        string.charAt(i) === "ã" ||
-        string.charAt(i) === "õ" ||
-        string.charAt(i) === "í" ||
-        string.charAt(i) === "á" ||
-        string.charAt(i) === "é" ||
-        string.charAt(i) === "ó" ||
-        string.charAt(i) === "à" ||
-        string.charAt(i) === "ê"){
+        if (string[i] === "a" ||
+        string[i] === "e" ||
+        string[i] === "i" ||
+        string[i] === "o" ||
+        string[i] === "u" ||
+        string[i] === "ã" ||
+        string[i] === "õ" ||
+        string[i] === "í" ||
+        string[i] === "á" ||
+        string[i] === "é" ||
+        string[i] === "ó" ||
+        string[i] === "à" ||
+        string[i] === "ê"){
             vogais += 1;
         }
     } 
     return vogais;
 }
-
-console.log(contaVogais('eu amo churros'))
-console.log(contaVogais('sssssss'))
-console.log(contaVogais('salve mano de boas'))
 
 function getTexto(){
     const frase = $text.value.toLowerCase();
@@ -37,7 +32,8 @@ function getTexto(){
 }
 
 function printResult(){
-    $resultado.textContent = contaVogais(getTexto()) + " 👌";
+    texto = getTexto();
+    $resultado.textContent = contaVogais(texto) + " 👌";
 }
 
 $button.addEventListener('click', function(){
